6321140af69f7b4b0fa24dd6accc94688378a1f2
Shong Lee <Shonglee17@gmail.com>
2023-05-09 16:47:03 -0500

    added notes to code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
